9ddb6a7a918343589376614bd9c4fb4084657ea1
AsuraHu <1030320304@qq.com>
2020-05-21 17:07:21 +0800

    tabBar

---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<!-- 自定义组件 -->
+    <navbar></navbar>
+    <view>
+      -- 内容
+    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
